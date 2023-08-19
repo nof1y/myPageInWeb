cacfa6ca1b98b8661ce46e99f3e821836881e506
nof11y <purplebatorg@gmail.com>
2023-08-19 14:40:08 +0300

    all was maked by zero

--- app_advertisements/apps.py
+++ app_advertisements/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppAdvertisementsConfig(AppConfig):
+class AppPageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'app_advertisements'
--- djangoPage/urls.py
+++ djangoPage/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_advertisements.urls')),
-    path('lesson_4', include('app_advertisements.urls')),
+    path('', include ('app_advertisements.urls')),
+    path('lesson_4', include ('app_lesson_4.urls')),
+    
 ]
