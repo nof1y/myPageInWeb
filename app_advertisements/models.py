7a7ea7fb105b2067006078b97fd99b2233f00e30
nof11y <purplebatorg@gmail.com>
2023-08-22 20:52:10 +0300

    update class Advertisements

--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
 class Advertisements(models.Model):
+    
     title = models.CharField('Заголовок', max_length = 128)
-    description = models.TextField('описание')
-    price = models.DecimalField('цена', max_digits = 10, decimal_places = 2)
-    auction = models.BooleanField('торг', help_text = 'отметьте если торг уместен')
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits = 10, decimal_places = 2)
+    auction = models.BooleanField('Торг', help_text = 'отметьте если торг уместен')
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+    
+    def __str__(self): 
+        return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
+    
+    class Meta: 
+        db_table = 'advertisements'
     
 
 # Create your models here.
