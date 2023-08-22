5dea4e138b643ea72967c431384ac4c1fdc579bb
nof11y <purplebatorg@gmail.com>
2023-08-22 19:37:16 +0300

    save the changes of lesson 7

--- app_advertisements/models.py
+++ app_advertisements/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Advertisements(models.Model):
+    title = models.CharField('Заголовок', max_length = 128)
+    description = models.TextField('описание')
+    price = models.DecimalField('цена', max_digits = 10, decimal_places = 2)
+    auction = models.BooleanField('торг', help_text = 'отметьте если торг уместен')
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    
+
 # Create your models here.
