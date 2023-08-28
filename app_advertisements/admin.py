07caf1e819914602efa32a8fe2c949f32286640b
nof11y <purplebatorg@gmail.com>
2023-08-28 21:24:02 +0300

    i was making a homework

--- app_advertisements/admin.py
+++ app_advertisements/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Advertisements
+
+
+class AdvertismentsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'price', 'auction', 'created_date', 'updated_date']
+    list_filter = ['auction', 'created_at']
+    
+    actions = ['make_auction_as_false', 'make_auction_as_true']
+    
+    @admin.action(description='Убрать возможность торга')
+    def make_auction_as_false(self, request, queryset):
+        queryset.update(auction=False)
+        
+    @admin.action(description='Добавить возможность торга')
+    def make_auction_as_true(self, request, queryset):
+        queryset.update(auction=True)
+        
+        
+    fieldsets = (
+        ('Общее', {
+            'fields': ('title', 'price'),
+        }),
+        ('Финансы', {
+            'fields': ('description', 'auction'),
+            'classes':['collapse']
+        })
+    )
+
+admin.site.register(Advertisements, AdvertismentsAdmin)
 
 # Register your models here.
